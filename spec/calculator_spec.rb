b19bcb3b15bc344a090172e2a6446954e55ae4b0
Graham Turner <turnertgraham@gmail.com>
2016-07-08 12:50:36 -0400

    completes multiply, divide, pow, sqry tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,6 +1,5 @@
 require "calculator"
 require "spec_helper"
-
 describe Calculator do
 
   let(:calc){Calculator.new}
@@ -49,12 +48,65 @@ describe Calculator do
       it "returns a float if the result is a decimal" do
         expect(calc.divide(3,2)).to eq(1.5)
       end
-      it "returns an integer if the result is a whole number" do
+      it "divides two positive numbers" do
         expect(calc.divide(6,2)).to eq(3)
       end
-      it "raises argument if dividing by zero"
+      it "returns an integer if the result is a whole number" do
+        expect(calc.divide(6,2)).to be_a(Fixnum)
+      end
+      it "raises argument if dividing by zero" do
+        expect{calc.divide(2,0)}.to raise_error(ArgumentError)
+      end
+    end
+
+    describe "#pow" do
+      it "raises a number to the correct power" do
+        expect(calc.pow(2,2)).to eq(4)
+      end
+      it "returns a float" do
+        expect(calc.pow(2,2)).to be_a(Float)
+      end
+      it "correctly raises a number to a negative power" do
+        expect(calc.pow(2,-2)).to eq(0.25)
+      end
+      it "correctly handles 0" do
+        expect(calc.pow(0, 0)).to eq(1)
+      end
+    end
+
+    describe "#sqrt" do
+      it "raises an argument if number is less than 0" do
+        expect{calc.sqrt(-1)}.to raise_error(ArgumentError)
+      end
+      it "gets the sqrt of a number" do
+        expect(calc.sqrt(4)).to eq(2)
+      end
+      it "returns integer if number is whole" do
+        expect(calc.sqrt(4)).to be_a(Fixnum)
+      end
+      it "output should be rounded to two places" do
+        expect(calc.sqrt(2)).to eq(1.41)
+      end
     end
   end
+
+  describe "#memory" do
+
+    describe "persistent memory" do
+      before(:all) { calc.memory = 8 }
+      
+    end
+
+
+    it "returns nil on new Calc object" do
+      expect(calc.memory).to be_nil
+    end
+
+    it "returns nil on new Calc object" do
+      expect(calc.memory).to be_nil
+    end
+  end
+
 
   describe "#output" do
     it "outputs string when stringify is true" do
